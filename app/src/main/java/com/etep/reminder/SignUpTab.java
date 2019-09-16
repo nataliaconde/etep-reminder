500cc13b7ccc4c5e00f8641c2a0d17e18f2b9ad2
Natália Condê <natalia.csouza98@outlook.com>
2019-09-15 22:43:55 -0300

    Adding lists

--- app/src/main/java/com/etep/reminder/SignUpTab.java
+++ app/src/main/java/com/etep/reminder/SignUpTab.java
@@ -1,9 +1,12 @@
 package com.etep.reminder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,7 +16,26 @@ public class SignUpTab extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstance){
-        return inflater.inflate(R.layout.sign_up_tab, container, false);
+        View view = inflater.inflate(R.layout.sign_up_tab, container, false);
+        Button btnLogin = (Button) view.findViewById(R.id.btnSignUp);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(
+                        getActivity(),
+                        "AQ",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        });
+
+        return view;
+    }
+
+
+    public void goToActivity(Class<?> view) {
+        Intent i = new Intent(getActivity(), view);
+        startActivity(i);
     }
 
 }
