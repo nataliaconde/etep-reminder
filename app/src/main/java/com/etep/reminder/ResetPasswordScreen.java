b7e6d09a5419a84c764504aec4e8bbea68e9fa5b
Natália Condê <natalia.csouza98@outlook.com>
2019-09-21 17:36:07 -0300

    Add linear gradient as backgroud

---
+++ app/src/main/java/com/etep/reminder/ResetPasswordScreen.java
@@ -0,0 +1,14 @@
+package com.etep.reminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPasswordScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password_screen);
+    }
+}
